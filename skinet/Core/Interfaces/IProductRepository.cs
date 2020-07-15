4c37bd082ba4ce24c669a9100911d06fda8280e4
Scott Phillips <Scott.Phillips@core-uk.com>
2020-07-15 10:40:45 +0100

    End of API architecture

--- skinet/Core/Interfaces/IProductRepository.cs
+++ skinet/Core/Interfaces/IProductRepository.cs
@@ -6,7 +6,10 @@ namespace Core.Interfaces
 {
     public interface IProductRepository
     {
+        //29. currently using one repository for all types...
          Task<Product> GetProductByIDAsync(int id);
          Task<IReadOnlyList<Product>> GetProductsAsync();
+         Task<IReadOnlyList<ProductBrand>> GetProductBrandsAsync();
+         Task<IReadOnlyList<ProductType>> GetProductTypesAsync();
     }
 }
--- skinet/Infrastructure/Data/ProductRepository.cs
+++ skinet/Infrastructure/Data/ProductRepository.cs
@@ -16,15 +16,35 @@ namespace Infrastructure.Data
         }
 
         //23. populate method to async find products and print out as list read only
+        //30. loading navigation properties with entity needed... 
         public async Task<IReadOnlyList<Product>> GetProductsAsync()
         {
-            return await _context.Products.ToListAsync();
+            return await _context.Products
+            .Include(p => p.ProductType)
+            .Include(p => p.ProductBrand)
+            .ToListAsync();
         }
 
         //23. populate method to async find product by id
+        //30. loading navigation properties with entity needed... 
+        //FindAsync() method does not accept 'I' queryable, so change to FirstOrDefault
         public async Task<Product> GetProductByIDAsync(int id)
         {
-            return await _context.Products.FindAsync(id);
+            return await _context.Products
+            .Include(p => p.ProductType)
+            .Include(p => p.ProductBrand)
+            .FirstOrDefaultAsync(p => p.Id == id);
+        }
+
+        //29.Implement ProductBrands and ProductTypes methods
+        public async Task<IReadOnlyList<ProductBrand>> GetProductBrandsAsync()
+        {
+            return await _context.ProductBrands.ToListAsync();
+        }
+
+        public async Task<IReadOnlyList<ProductType>> GetProductTypesAsync()
+        {
+            return await _context.ProductTypes.ToListAsync();
         }
     }
 }
