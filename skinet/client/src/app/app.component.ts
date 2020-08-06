6ae331252915684472b95f3775ae487b225db6c1
Scott Phillips <Scott.Phillips@core-uk.com>
2020-08-06 11:43:44 +0100

    Product Interfaces created

--- skinet/client/src/app/app.component.ts
+++ skinet/client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import {IProduct} from './models/product';
+import {IPagination} from './models/IPagination';
 
 @Component({
   selector: 'app-root',
@@ -7,13 +9,13 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  title = "Scott's Ski Site";
-  products: any[];
+  title = 'Scotts Ski Site';
+  products: IProduct[];
 
   constructor(private http: HttpClient){}
 
   ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe((response: any) => {
+    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe((response: IPagination) => {
       this.products = response.data;
     }, error => {
       console.log(error);
