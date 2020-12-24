6f77df0acdb9236b161bed347072e1d612a34b48
Scott Phillips <Scott.Phillips@core-uk.com>
2020-12-24 12:23:57 +0000

    Working 'view order' route

--- skinet/client/src/app/orders/order-detailed/order-detailed.component.ts
+++ skinet/client/src/app/orders/order-detailed/order-detailed.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { IOrder } from 'src/app/shared/models/order';
+import { ActivatedRoute } from '@angular/router';
+import { BreadcrumbService } from 'xng-breadcrumb';
+import { OrdersService } from '../orders.service';
 
 @Component({
   selector: 'app-order-detailed',
@@ -6,10 +10,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./order-detailed.component.scss']
 })
 export class OrderDetailedComponent implements OnInit {
+  order: IOrder;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private breadcrumbService: BreadcrumbService, private ordersService: OrdersService) {
+    this.breadcrumbService.set('@OrderDetailed', '');
+  }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.ordersService.getOrderDetailed(+this.route.snapshot.paramMap.get('id'))
+      .subscribe((order: IOrder) => {
+      this.order = order;
+      this.breadcrumbService.set('@OrderDetailed', `Order# ${order.id} - ${order.status}`);
+    }, error => {
+      console.log(error);
+    });
   }
 
 }
--- skinet/client/src/app/orders/orders.module.ts
+++ skinet/client/src/app/orders/orders.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OrdersComponent } from './orders.component';
 import { OrderDetailedComponent } from './order-detailed/order-detailed.component';
-
-
+import { OrdersRoutingModule } from './orders-routing.module';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [OrdersComponent, OrderDetailedComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    OrdersRoutingModule,
+    SharedModule
   ]
 })
 export class OrdersModule { }
