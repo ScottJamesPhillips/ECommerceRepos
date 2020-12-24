4eed21ba07a31b1613d674834f58d0f59218e75d
Scott Phillips <Scott.Phillips@core-uk.com>
2020-12-24 10:50:42 +0000

    Added orders and ordered-detail folders

--- skinet/client/src/app/app-routing.module.ts
+++ skinet/client/src/app/app-routing.module.ts
@@ -8,24 +8,34 @@ import { ServerErrorComponent } from './core/test-error/server-error/server-erro
 import { NotFoundComponent } from './core/test-error/not-found/not-found.component';
 import { AuthGuard } from '../app/core/guards/auth.guard';
 
-
 const routes: Routes = [
   // Path - home component (localhost:4200/)
-  {path: '', component: HomeComponent, data: {breadcrumb: 'Home'}},
-  {path: 'test-error', component: TestErrorComponent, data: {breadcrumb: 'Test Errors'}},
-  {path: 'server-error', component: ServerErrorComponent, data: {breadcrumb: 'Server Error'}},
-  {path: 'not-found', component: NotFoundComponent, data: {breadcrumb: 'Not Found'}},
+  { path: '', component: HomeComponent, data: { breadcrumb: 'Home' } },
+  { path: 'test-error', component: TestErrorComponent, data: { breadcrumb: 'Test Errors' } },
+  { path: 'server-error', component: ServerErrorComponent, data: { breadcrumb: 'Server Error' } },
+  { path: 'not-found', component: NotFoundComponent, data: { breadcrumb: 'Not Found' } },
   // Path - shop component (localhost:4200/shop)
-  {path: 'shop', loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule),
-  data: {breadcrumb: 'Shop'}},
-  {path: 'basket', loadChildren: () => import('./basket/basket.module').then(mod => mod.BasketModule),
-  data: {breadcrumb: 'Basket'}},
-  {path: 'checkout', canActivate:[AuthGuard], loadChildren: () => import('./checkout/checkout.module').then(mod => mod.CheckoutModule),
-  data: {breadcrumb: 'Checkout'}},
-  {path: 'account', loadChildren: () => import('./account/account.module').then(mod => mod.AccountModule),
-  data: {breadcrumb: {skip: true}}},
+  { path: 'shop', loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule), data: { breadcrumb: 'Shop' } },
+  { path: 'basket', loadChildren: () => import('./basket/basket.module').then(mod => mod.BasketModule), data: { breadcrumb: 'Basket' } },
+  {
+    path: 'checkout',
+    canActivate: [AuthGuard],
+    loadChildren: () => import('./checkout/checkout.module')
+      .then(mod => mod.CheckoutModule), data: { breadcrumb: 'Checkout' }
+  },
+  {
+    path: 'orders',
+    canActivate: [AuthGuard],
+    loadChildren: () => import('./orders/orders.module')
+      .then(mod => mod.OrdersModule), data: { breadcrumb: 'Orders' }
+  },
+  {
+    path: 'account',
+    loadChildren: () => import('./account/account.module')
+      .then(mod => mod.AccountModule), data: { breadcrumb: { skip: true } }
+  },
   // Path - redirect to home if URL incorrect
-  {path: '**', redirectTo: 'not-found', pathMatch: 'full' }
+  { path: '**', redirectTo: 'not-found', pathMatch: 'full' }
 ];
 
 @NgModule({
